0e0a65d8649b9ddde7ee1a6aaa7b9d6fbccb0f12
Binay Sahoo <binay.ranjan@gmail.com>
2020-05-29 04:16:45 +0530

    adding again src foolder with .java

---
+++ MyTestProject/src/com/test/TestClass.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class TestClass {
+	public static void main(String []args) {
+		System.out.println("My Test Project");
+		System.out.println("Updated Code");
+		System.out.println("Updated after sometime");
+		System.out.println("Updated after deletion");
+	}
+}

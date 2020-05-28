bc0b36f6963a64a69bfe362e8186482c9f91c3d4
Binay Sahoo <binay.ranjan@gmail.com>
2020-05-29 04:13:14 +0530

    Unknwon chnages

--- MyTestProject/src/com/test/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class TestClass {
-	public static void main(String []args) {
-		System.out.println("My Test Project");
-		System.out.println("Updated Code");
-		System.out.println("Added Recently");
-	}
-}

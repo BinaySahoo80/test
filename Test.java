7d93953954e647ae8c01a951830ee6b45becd137
BinaySahoo80 <66088491+BinaySahoo80@users.noreply.github.com>
2020-06-03 02:27:01 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	public static void main(String []args) {
+		System.out.println("My Test Project");
+		System.out.println("Updated Code");
+		System.out.println("Updated after sometime");
+		System.out.println("Updated after deletion");
+		System.out.println("Updated after adding again");
+		System.out.println("Adding in GitHub");
+		System.out.println("Adding in GitHub on 29th May 2020");
+		System.out.println("Editing in GitHub on 29th May 2020 for NewBranchforTest");
+		System.out.println("Updating in NewBranch");
+	}
+}
